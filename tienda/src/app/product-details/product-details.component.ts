dc0e399aa56d434a7e9e056aa0f5118833cc816f
JuanArboledaL <juanarboledalopez@gmail.com>
2025-02-18 11:09:13 +0100

    examen

--- tienda/src/app/product-details/product-details.component.ts
+++ tienda/src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Product, products } from '../products';
+import { Product,products } from '../products';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -14,7 +14,9 @@ import { CartService } from '../cart.service';
 export class ProductDetailsComponent {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { 
+
+  }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
@@ -25,7 +27,7 @@ export class ProductDetailsComponent {
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
-  
+
     // Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
   }
--- tienda/src/app/products.ts
+++ tienda/src/app/products.ts
@@ -3,6 +3,9 @@ export interface Product {
     name: string;
     price: number;
     description: string;
+    estock:number;
+    caracteristicas: string[];
+    garantia: string
   }
   
   export const products = [
@@ -10,18 +13,36 @@ export interface Product {
       id: 1,
       name: 'Phone XL',
       price: 799,
-      description: 'A large phone with one of the best screens'
+      description: 'A large phone with one of the best screens',
+      estock: 10,
+      caracteristicas: ["5G","Pantalla OLED","Almacenamiento de 128GB"],
+      garantia:{
+        periodo:"2 Años",
+        tipo: "Fabricante"
+      }
     },
     {
       id: 2,
       name: 'Phone Mini',
       price: 699,
-      description: 'A great phone with one of the best cameras'
+      description: 'A great phone with one of the best cameras',
+      estock: 0,
+      caracteristicas: ["5G","Pantalla OLED","Almacenamiento de 500GB"],
+      garantia:{
+        periodo:"3 Años",
+        tipo: "Fabricante"
+      }
     },
     {
       id: 3,
       name: 'Phone Standard',
       price: 299,
-      description: ''
+      description: '',
+      estock: 2,
+      caracteristicas: [],
+      garantia:{
+        periodo:"2 Años",
+        tipo: "Fabricante"
+      }
     }
   ];
